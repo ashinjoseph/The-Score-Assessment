10d67950e4c734b1f3553b35e2f59b961c452803
ashin <ashin.joseph@morningstar.com>
2024-04-11 16:18:46 -0400

    Updated Framework to use saucelab

--- src/test/java/org/the/score/common/test/BaseTest.java
+++ src/test/java/org/the/score/common/test/BaseTest.java
@@ -6,6 +6,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
@@ -27,6 +28,8 @@ public class BaseTest {
     protected final static String APP = "C:/Ashin/Appium/theScore.apk";
     protected final static String APPIUM = "http://localhost:4723/";
 
+    protected String Execution_Environment;
+
     protected AndroidDriver driver;
     protected CommonActions actions;
 
@@ -37,22 +40,48 @@ public class BaseTest {
     }
     @BeforeSuite(alwaysRun = true)
     public void beforeSuite(ITestContext context) {
+
         FilesUtility.init();
+
     }
 
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        caps.setCapability(MobileCapabilityType.APP, APP);
+    @BeforeTest
+    public void setUp(ITestContext context) throws MalformedURLException {
+
+        Execution_Environment = context.getCurrentXmlTest().getParameter("execution-env");
+        DesiredCapabilities caps;
+
+        switch (Execution_Environment) {
+            case "saucelab":
+                caps = new DesiredCapabilities();
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("appium:app", "storage:filename=mda-2.0.1-22.apk");  // The filename of the mobile app
+                caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+                caps.setCapability("appium:platformVersion", "12.0");
+                caps.setCapability("appium:automationName", "UiAutomator2");
+                MutableCapabilities sauceOptions = new MutableCapabilities();
+                sauceOptions.setCapability("username", "oauth-ashinjoseph.joseph-e773a");
+                sauceOptions.setCapability("accessKey", "23075c50-48d5-4839-a235-90d7a8b77e3b");
+                sauceOptions.setCapability("build", "appium-build-GW13D");
+                sauceOptions.setCapability("name", "<your test name>");
+                sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
+                caps.setCapability("sauce:options", sauceOptions);
+
+            default:
+                caps = new DesiredCapabilities();
+                caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+                caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
+                caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+                caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                caps.setCapability(MobileCapabilityType.APP, APP);
+
+        }
         driver = new AndroidDriver(new URL(APPIUM), caps);
 
+
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         if (driver != null) {
             driver.quit();
